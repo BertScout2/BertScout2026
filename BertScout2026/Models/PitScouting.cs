953aa79887bf21ad067cde94cf052689b317f868
BertScout <bertscout2@outlook.com>
2026-01-27 19:37:00 -0500

    Added pit scouting model

---
+++ BertScout2026/Models/PitScouting.cs
@@ -0,0 +1,18 @@
+﻿namespace BertScout2026.Models
+{
+    internal class PitScouting
+    {
+        // Meta data
+        public int TeamNumber { get; set; }
+
+        // Robot properties
+        public int ClimbingLevel { get; set; }
+        public string? PreferredStartingPosition { get; set; }
+        public string? DriveType { get; set; }
+        public int BallCapacity { get; set; }
+        public int LaunchingSpeed { get; set; }
+        public int RobotSpeed { get; set; }
+        public int NumberOfAutonomousPrograms { get; set; }
+        public int NumberOfTeamMembers { get; set; }
+    }
+}
