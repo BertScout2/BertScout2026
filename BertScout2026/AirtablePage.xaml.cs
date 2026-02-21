9689d436a95a9fab59a82d3ed9662ec2f30e5d74
BertScout <bertscout2@outlook.com>
2026-02-21 15:41:23 -0500

    flyout pages

---
+++ BertScout2026/AirtablePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace BertScout2026;
+
+public partial class AirtablePage : ContentPage
+{
+	public AirtablePage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ BertScout2026/MatchSchedulePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace BertScout2026;
+
+public partial class MatchSchedulePage : ContentPage
+{
+	public MatchSchedulePage()
+	{
+		InitializeComponent();
+	}
+}
