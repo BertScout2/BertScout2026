66cf6a88180389f6dcb5708d7e06acdd6e5adfff
Bert-03 <bertscout2@outlook.com>
2026-02-25 20:43:53 -0500

    Added comment picker

--- BertScout2026/MainPage.xaml.cs
+++ BertScout2026/MainPage.xaml.cs
@@ -27,6 +27,11 @@ namespace BertScout2026
                 ShowError("Storage Permissions have been denied\n" +
                     "Please turn on Storage permission in App Info / Permissions");
             }
+            CommentPicker.Items.Clear();
+            foreach (string s in CommentList)
+            {
+                CommentPicker.Items.Add(s);
+            }
         }
 
         private void StartButtonClicked(object? sender, EventArgs e)
@@ -576,7 +581,23 @@ namespace BertScout2026
         }
 
         #endregion
-
+        private readonly List<string> CommentList =
+        [
+            "Can dislodge algae.",
+                "Can pick from the floor.",
+                "Can only handle algae.",
+                "Got a coral stuck inside.",
+                "Missed shooting algae into barge.",
+                "Dropped coral a lot.",
+                "Dropped algae a lot.",
+                "Tried to hang but failed.",
+                "Played defense.",
+                "Caused a penalty.",
+                "Had technical issues.",
+                "Broke down.",
+                "Never moved.",
+                "DON'T PICK!",
+            ];
         #region Score
 
         private void ScoreStar0Clicked(object? sender, EventArgs e)
@@ -641,6 +662,19 @@ namespace BertScout2026
             MainLayout.IsVisible = false;
             ErrorLayout.IsVisible = true;
             ErrorMsg.Text = message;
+        }
+
+        private void CommentPicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (CommentPicker.SelectedIndex < 0)
+                return;
+            if (Comments.Text == null)
+                Comments.Text = "";
+            else if (Comments.Text.Length > 0 && !Comments.Text.EndsWith(' '))
+                Comments.Text += " ";
+            Comments.Text += CommentPicker.SelectedItem.ToString() + " ";
+            CommentPicker.SelectedIndex = -1;
+            SaveData();
         }
     }
 }
