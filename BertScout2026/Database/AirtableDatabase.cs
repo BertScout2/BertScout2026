3bf50e63e6a44c2d2eb86c506ae9ee84d4222319
Scott Bakker <sbakker343@gmail.com>
2026-03-02 23:21:42 -0500

    airtable tokens

---
+++ BertScout2026/Database/AirtableDatabase.cs
@@ -0,0 +1,8 @@
+﻿namespace BertScout2026.Database;
+
+public class AirtableDatabase
+{
+    private const string AIRTABLE_BASE = "appZplIjMnsIy50Ku";
+    private const string AIRTABLE_TABLE = "tbloentIMHSxGJRjX";
+    private const string AIRTABLE_TOKEN_BASE64 = "cGF0WjJKR05Kak80VXE2SEEuNWQ5YzFkZWQyM2Q1NzAwNTgyMTJkYjUzYWYzZTY1MDBhNzgyZGI2Mzg4NzRkMjVmN2QzMTEzZTI4NjBjYTdhNA==";
+}
